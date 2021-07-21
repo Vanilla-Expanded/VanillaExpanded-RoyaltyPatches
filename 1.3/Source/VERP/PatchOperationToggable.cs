05520631ed91981494f731387ca01fde9305f9ca
Kylian <kylian.balde@gmail.com>
2021-07-21 19:49:52 +0200

    1.3 tag and 1.3 source folder

---
+++ 1.3/Source/VERP/PatchOperationToggable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using Verse;
+
+namespace VERP
+{
+    public class PatchOperationToggable : PatchOperation
+	{
+#pragma warning disable 0649
+		public bool enabled;
+		public string label;
+		public PatchOperation match;
+
+		protected override bool ApplyWorker(XmlDocument xml)
+		{
+			if (this.enabled)
+			{
+				if (this.match != null)
+				{
+					return this.match.Apply(xml);
+				}
+			}
+			return true;
+		}
+	}
+}
